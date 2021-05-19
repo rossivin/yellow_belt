d41ff5b2fc61b97393182db1844818bf046e7102
Vinicius Rossi <rossi.vinicius@gmail.com>
2021-05-19 14:26:12 -0400

    FilterVessel Class Created

---
+++ filter_vessel.py
@@ -0,0 +1,6 @@
+class FilterVessel():
+
+	def __init__(self, volume, pre_line_volume = 0, post_line_volume = 0):
+		self.volume = volume
+		self.pre_line_volume = pre_line_volume
+		self.post_line_volume = post_line_volume
